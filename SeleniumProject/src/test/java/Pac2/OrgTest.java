521c8d57bad7a79eab2c571ba9577613f8b0546b
Sauravgarg <Sauravgarg@LAPTOP-AG5AQ5SQ.mshome.net>
2024-09-03 10:52:39 +0530

    First push

---
+++ SeleniumProject/src/test/java/Pac2/OrgTest.java
@@ -0,0 +1,16 @@
+package Pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void creatOrgTest() {
+		System.out.println("Execute createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("Execute OrgTest");
+	}
+	
+}
